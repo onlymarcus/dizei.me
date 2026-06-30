d9d644383bdc9f225e960af1c63f8f201e67111f
Marcus <marcusjunior@gmail.com>
2026-06-30 20:59:08 -0300

    quero implementar dizei

--- lib/site-config.ts
+++ lib/site-config.ts
@@ -7,8 +7,8 @@ export const demoWhatsappHref =
   "https://wa.me/5581999151979?text=ol%C3%A1%2C%20tudo%20bem";
 
 export const commercialWhatsappHref =
-  "https://wa.me/5581999440909?text=Ol%C3%A1%2C%20quero%20implementar%20o%20Dizei%20na%20minha%20cl%C3%ADnica.";
-export const commercialWhatsappDisplay = "+55 81 99944-0909";
+  "https://wa.me/5581999151979?text=Ol%C3%A1%2C%20quero%20implementar%20o%20Dizei%20na%20minha%20cl%C3%ADnica.";
+export const commercialWhatsappDisplay = "+55 81 99915-1979";
 
 export const calendarEmbedUrl =
   "https://calendar.google.com/calendar/embed?src=61a30ff6ddf982134fa08cfc7a00e64c846c17b9e7d489f65f4982efbd636557%40group.calendar.google.com&ctz=America%2FRecife";
